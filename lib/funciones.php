a4fcbf9ebee6cb5f6d7fc4bd6e1e28cc91357ad9
kevin-lozoya <kevinlozoyaginer@gmail.com>
2018-02-07 11:20:14 +0100

    Agregada función postParamsExists

--- lib/funciones.php
+++ lib/funciones.php
@@ -5,5 +5,36 @@ function smarty(){
     return $plantilla;
 }
 
+// Funcionamiento de postparamsExists
+// $keysPost = array(
+// 	'a' => 'idApp',
+// 	't' => 'tokenDisp',
+// 	'e' => 'empresa',
+// 	'so' => 'sistemaOperativo',
+// 	'desReg' => 'borrarRegistro'
+// );
+function postParamsExists($keysPost) {
+  $valido = true;
+  
+  $params = array();
+
+  foreach ($keysPost as $keyPost => $keyParams) {
+    if (!isset($_POST[$keyPost])){
+      $valido = false;
+      break;
+    }
+    
+    $params[$keyParams] = $_POST[$keyPost];
+  }
+
+  if (!$valido) {
+    apiError('No se pudo procesar la petición', false);
+    http_response_code(422);
+    return null;
+  }
+
+  return $params;
+}
+
 
 ?>
